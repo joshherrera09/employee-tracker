a302afad54f381e2c27a111983cb1600e3bc66de
Josh Herrera <joshherrera09@gmail.com>
2020-09-22 13:51:29 -0700

    init(); defined as initial prompts with switch statement

--- server.js
+++ server.js
@@ -1,15 +1,71 @@
+// Dependencies
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+// Connection to mySql workbench
 var connection = mysql.createConnection({
     host: "localhost",
+    // PORT
     port: 8889,
+    // Username
     user: "root",
+    // Password
     password: "root",
+    // Database
     database: "employee_trackerDB"
 });
 
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-})
+    init();
+});
+
+function init() {
+    inquirer
+        .prompt({
+            message: "What would you like the employee tracker to help you with?",
+            type: "list",
+            choices: [
+                "Add department",
+                "Add role",
+                "Add employee",
+                "View Departments",
+                "View Roles",
+                "View employees",
+                "I'm done"
+            ],
+            name: "todo"
+        }).then(function(res) {
+            switch (res.todo) {
+                case "Add department":
+                    addDepartment();
+                    break;
+
+                case "Add role":
+                    addRole();
+                    break;
+
+                case "Add employee":
+                    addEmployee();
+                    break;
+                
+                case "View Departments":
+                    viewDepartments();
+                    break;
+
+                case "View Roles":
+                    viewRoles();
+                    break;
+
+                case "View employees":
+                    viewEmployees();
+                    break;
+
+                case "I'm done":
+                    connection.end();
+                    break;
+
+            }
+        })
+}
